4f82875045155dc1060b6a6971503506f4c8b965
anoop <anoopm6@gmail.com>
2018-07-26 19:31:16 +0530

    initial commit

---
+++ api/models/userListModel.js
@@ -0,0 +1,23 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema   = mongoose.Schema
+
+var userSchema = new Schema({
+  name: {
+    type: String,
+    required: 'Kindly enter the name of the task'
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  profession: {
+    type: String,
+  },
+  userId: {
+    type: String,
+  }
+});
+
+module.exports = mongoose.model('Users', userSchema);
---
+++ server.js
@@ -0,0 +1,20 @@
+var express = require('express'),
+app = express(),
+port = process.env.PORT || 3000,
+mongoose = require('mongoose'),
+Task = require('./api/models/userListModel'),
+bodyParser = require('body-parser');
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/usersDb');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./api/routes/usersListRoutes'); //importing route
+routes(app); //register the route
+
+app.listen(port);
+
+console.log('userlist list RESTful API server started on: ' + port);
